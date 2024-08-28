31392934809ac67a66708989d4534b792989ffbc
Oybek <oybek@inbl.io>
2024-08-28 19:03:10 +0900

    feat: add image and item cards to the tech section

--- components/ui/Grid.tsx
+++ components/ui/Grid.tsx
@@ -1,12 +1,18 @@
+import { gridItems } from '@/data';
 import { BentoGrid, BentoGridItem } from './BentoGrid';
 
 const Grid = () => {
     return (
         <section id='about'>
             <BentoGrid>
-                {[{ title: 'Title', description: 'desc', id: 1 }].map((item, i) => (
-                    <BentoGridItem id={item.title} key={item.id} title={item.title} description={item.description} />
-                ))}
+                {gridItems.map(
+                    ({ id, title, description, className, img, titleClassName, imgClassName, spareImg }, i) => (
+                        <BentoGridItem
+                            key={id}
+                            {...{ id, title, description, className, img, titleClassName, imgClassName, spareImg }}
+                        />
+                    )
+                )}
             </BentoGrid>
         </section>
     );
